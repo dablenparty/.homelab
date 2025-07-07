bf6d741a3c6eeceb432c9cc0171653a1e382ad1a
dablenparty <anoptimisticcouch@gmail.com>
2025-07-07 19:44:43 -0400

    refactor(traefik): clean up decode_certs.py

--- services/traefik/decode_certs.py
+++ services/traefik/decode_certs.py
@@ -6,13 +6,22 @@ import os
 
 from pathlib import Path
 
+def decode_to_pem(b64_key: str, out_path: Path):
+    decoded = base64.b64decode(b64_key)
+    if out_path.exists():
+        print(f"removing existing {out_path.name}")
+        out_path.unlink()
+    bytes_written = out_path.write_bytes(decoded)
+    print(f"wrote {bytes_written}B to {out_path}")
+
+
 if __name__ == "__main__":
     if os.geteuid() != 0:
         raise PermissionError("Please re-run this script as root!")
     base_dir = Path(__file__).parent
     acme_path = base_dir / "config" / "acme.json"
     out_path = base_dir / "certs"
-    out_path.mkdir()
+    out_path.mkdir(exist_ok=True)
     with acme_path.open("r") as fd:
         acme_json = json.load(fd)
     for key in acme_json:
@@ -22,12 +31,8 @@ if __name__ == "__main__":
             continue
         for cert_info in certs:
             domain = cert_info["domain"]["main"]
-            decoded_cert = base64.b64decode(cert_info["certificate"])
-            decoded_key = base64.b64decode(cert_info["key"])
-            cert_path = out_path / f"{domain}.cert"
-            key_path = out_path / f"{domain}.key"
-            bytes_written = cert_path.write_bytes(decoded_cert)
-            print(f"wrote {bytes_written}B to {cert_path}")
-            bytes_written = key_path.write_bytes(decoded_key)
-            print(f"wrote {bytes_written}B to {key_path}")
+            cert_path = out_path / f"{domain}-cert.pem"
+            decode_to_pem(cert_info["certificate"], cert_path)
+            key_path = out_path / f"{domain}-key.pem"
+            decode_to_pem(cert_info["key"], key_path)
 
