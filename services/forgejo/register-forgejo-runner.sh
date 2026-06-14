ec84ed8dd38dc7f474a3e189c0ac8f0edf4781ea
dablenparty <anoptimisticcouch@gmail.com>
2026-06-14 17:15:14 -0400

    fix(forgejo): docker runner now registers

--- services/forgejo/register-forgejo-runner.sh
+++ services/forgejo/register-forgejo-runner.sh
@@ -7,7 +7,7 @@ set -a && source .env && set +a
 
 forgejo-runner create-runner-file --connect --instance http://forgejo-server:3000 --name runner --secret "${SHARED_SECRET}"
 # add 'docker' label
-sed -i -e "s|\"labels\": null|\"labels\": [\"docker:docker://code.forgejo.org/oci/node:20-bookworm\"]|" .runner
+sed -i -e "s|\"labels\": \[\]|\"labels\": [\"docker:docker://code.forgejo.org/oci/node:20-bookworm\"]|" .runner
 forgejo-runner generate-config >config.yml
 # set runner config vars
 sed -i -e "s|^  envs:$$|  envs:\n    DOCKER_HOST: $DOCKER_HOST|" -e "s|network: .*|network: forgejo-net|" -e "s|  valid_volumes: \[\]$$|  valid_volumes:\n    - \"**\"|" -e "s|  docker_host: .*|  docker_host: \"$DOCKER_HOST\"|" config.yml
